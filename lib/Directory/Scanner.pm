7eada1de55b5b26d87e57898e608a93acacc3c26
Stevan Little <stevan.little@gmail.com>
2017-08-31 01:34:33 +0200

    notes

--- lib/Directory/Scanner.pm
+++ lib/Directory/Scanner.pm
@@ -37,6 +37,7 @@ sub concat {
 
 ## builder instance methods
 
+# XXX - I kind of would like to get rid of this, you'll always do it, I think.
 sub recurse {
 	my ($builder) = @_;
 	push @$builder => [ 'Directory::Scanner::Stream::Recursive' ];
@@ -45,10 +46,13 @@ sub recurse {
 
 sub ignore {
     my ($builder, $filter) = @_;
+    # XXX - make this a module, not a hack-up of Filtered
+    # XXX - should this support using at .gitignore files?
     push @$builder => [ 'Directory::Scanner::Stream::Filtered', filter => sub { !$filter->( @_ ) } ];
     return $builder;
 }
 
+# XXX - consider renaming filter to match, it is more appropriate ...
 sub filter {
 	my ($builder, $filter) = @_;
 	push @$builder => [ 'Directory::Scanner::Stream::Filtered', filter => $filter ];
@@ -60,6 +64,8 @@ sub apply {
 	push @$builder => [ 'Directory::Scanner::Stream::Application', f => $f ];
 	return $builder;
 }
+
+## builder method
 
 sub stream {
 	my ($builder) = @_;
@@ -100,28 +106,15 @@ __END__
 	# get all entries in a directory recusively
 	# and filter out anything that is not a directory
 
-    # NOTE:
-    # A filter after a recurse will
-    # iterate over every file in the
-    # entire tree.
-
 	Directory::Scanner->for( $dir )
 					  ->recurse
 					  ->filter(sub { (shift)->is_dir })
 					  ->stream;
 
-	# get all entries in a directory, filter out
-	# anything that is a . directory, then recurse
-
-    # NOTE:
-    # A filter before a recurse will
-    # be used to descend each level of
-    # the tree, such that it guards
-    # which directories are recursed
-    # into and which that are not.
+	# ignore anything that is a . directory, then recurse
 
 	Directory::Scanner->for( $dir )
-					  ->filter(sub { (shift)->basename =~ /^\./ })
+					  ->ignore(sub { (shift)->basename =~ /^\./ })
 					  ->recurse
 					  ->stream;
 
